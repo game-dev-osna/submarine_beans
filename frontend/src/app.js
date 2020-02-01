afb6ee5bc03b58c2a9b27b3fdea1fdf703ae8a8f
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2020-02-01 02:08:45 +0100

    js autocompletion

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,33 +2,51 @@ import { lol } from './some_file.js'
 
 console.log('asd', lol);
 
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 400,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+window
+
+class SimpleGame {
+
+    /** @member {Phaser.Loader} load */
+
+    constructor() {
+
+        var config = {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 400,
+            scene: {
+                preload: this.preload,
+                create: this.create,
+                update: this.update
+            }
+        };
+
+        this.game = new Phaser.Game(config);
     }
+
+    preload() {
+        /** @type {Phaser.State} */
+        const self = this;
+
+        self.load.image('sky', 'assets/sky.png');
+        self.load.image('ground', 'assets/platform.png');
+        self.load.image('star', 'assets/star.png');
+        self.load.image('bomb', 'assets/bomb.png');
+        self.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    create() {
+        /** @type {Phaser.State} */
+        const self = this;
+
+        self.add.image(400, 300, 'sky');
+        self.add.image(0, 0, 'sky').setOrigin(0, 0);
+    }
+
+    update() {
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
 };
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-}
-
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-}
-
-function update ()
-{
-}
